ad626f097f96448786fbf4af86b3396ff65e6726
SihanHsiang <159276157+SihanHsiang@users.noreply.github.com>
2024-02-29 11:54:01 +0000

    Add files via upload

---
+++ hello_world.c
@@ -0,0 +1,11 @@
+#include <stdio.h> // directive to include 
+
+int main(void)   // function which is run when the programs starts 
+{ 
+printf("Hello World"); // our statement/command to run 
+
+int i = -4; // make a variable called i 
+i = i + 3; 
+assert(i > 2);
+    return 0; // the return statement for the function main 
+} 
